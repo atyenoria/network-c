61e2e02ad9a7576f1e2cb4acacc523a1ac4038de
atyenoria <eigotyoubunn30@gmail.com>
2016-07-15 11:14:04 +0900

    mutex

---
+++ thread/mutex1.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+ 
+const size_t    loop_max = 65535;
+ 
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+int counter = 0;
+ 
+void f1();
+void f2();
+ 
+int
+main(int argc, char *argv[])
+{
+        pthread_t thread1, thread2;
+        int ret1,ret2;
+ 
+        ret1 = pthread_create(&thread1,NULL,(void *)f1,NULL);
+        ret2 = pthread_create(&thread2,NULL,(void *)f2,NULL);
+ 
+        if (ret1 != 0) {
+                err(EXIT_FAILURE, "can not create thread 1: %s", strerror(ret1) );
+        }
+        if (ret2 != 0) {
+                err(EXIT_FAILURE, "can not create thread 2: %s", strerror(ret2) );
+        }
+ 
+        printf("execute pthread_join thread1\n");
+        ret1 = pthread_join(thread1,NULL);
+        if (ret1 != 0) {
+                errc(EXIT_FAILURE, ret1, "can not join thread 1");
+        }
+ 
+        printf("execute pthread_join thread2\n");
+        ret2 = pthread_join(thread2,NULL);
+        if (ret2 != 0) {
+                errc(EXIT_FAILURE, ret2, "can not join thread 2");
+        }
+ 
+        printf("done\n");
+        printf("%d\n", counter);
+ 
+        pthread_mutex_destroy(&m);
+        return 0;
+}
+ 
+void
+f1()
+{
+        size_t i;
+ 
+        for(i=0; i<loop_max; i++){
+#ifndef NOLOCK
+                int r;
+                r = pthread_mutex_lock(&m);
+                if (r != 0) {
+                        errc(EXIT_FAILURE, r, "can not lock");
+                }
+#endif
+                counter++;
+#ifndef NOLOCK
+                r = pthread_mutex_unlock(&m);
+                if (r != 0) {
+                        errc(EXIT_FAILURE, r, "can not unlock");
+                }
+#endif
+        }
+}
+ 
+void
+f2()
+{
+        size_t i;
+ 
+        for(i=0; i<loop_max; i++){
+#ifndef NOLOCK
+                if (pthread_mutex_lock(&m) != 0) {
+                        err(EXIT_FAILURE, "can not lock");
+                }
+#endif
+                counter++;
+#ifndef NOLOCK
+                if (pthread_mutex_unlock(&m) != 0) {
+                        err(EXIT_FAILURE, "can not unlock");
+                }
+#endif
+        }
+}
